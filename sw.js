80c2a34867e400e8f7c9dc732e828b8637f35051
Hxxmolly <hxxmolly@users.noreply.github.com>
2026-07-27 11:11:56 +0800

    学习工作台 v1.0 - WPS二级+六级备考

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'workstation-v8-zindex';
+const ASSETS = [
+  '/',
+  '/creator-workstation.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res.ok && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
